6e31c016081e57198015d9280ff5de82fe21996f
BradleyParkerDev <bradleyscottparker@outlook.com>
2022-10-19 11:38:25 -0500

    updated

--- stringSpace.js
+++ stringSpace.js
@@ -8,6 +8,21 @@ console.log(stringSpace(str3));
 
 function stringSpace(str){
     let newString = '';
+    for(let i = 0; i < str.length; i++){
+        if(i < (str.length)){ //change to str.length - 1 to use else
+            newString = newString + str[i] + " ";
+        }
+    //     else{
+    //         newString = newString + str[i];
+    //     }
+    }
+    return newString.substring(0,newString.length-1)
+
+    // return newString;
+}
+
+// function stringSpace(str){
+    // let newString = '';
     //let myArray = [];
     // for(let i = 0; i < str.length; i++){
     //     myArray.push(str[i]);
@@ -15,14 +30,4 @@ function stringSpace(str){
     // newString = myArray.join(" ");
     // return newString;
 
-    for(let i = 0; i < str.length; i++){
-        if(i < (str.length-1)){
-            newString = newString + str[i] + " ";
-        }
-        else{
-            newString = newString + str[i];
-        }
-    }
-    //console.log(newString.length);
-    return newString;
-}
+// }
